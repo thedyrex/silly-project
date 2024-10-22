dae369fd4845cc02f1baf3cc7de7e51e4aa11311
Lucas Doell <lucas@lux.dev>
2024-10-22 16:35:11 -0400

    Refactor to Next 15 and use TypeScript

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from "tailwindcss";
+
+const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -7,10 +8,12 @@ module.exports = {
   ],
   theme: {
     extend: {
-      fontFamily: {
-        'sequel-sans': ['SequelSansBoldHead', 'sans-serif'],
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
       },
     },
   },
   plugins: [],
 };
+export default config;
